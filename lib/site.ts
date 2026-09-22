b8666b167179f0efcaee88f4d2cdd168504da98d
Clinton Omondi <kingclinn40931@gmail.com>
2026-09-22 12:44:56 +0300

    feat: update site.ts for canonilised url

---
+++ lib/site.ts
@@ -0,0 +1,5 @@
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_SITE_URL ||
+  (process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "https://tmocu.kingclinn40931.workers.dev");
