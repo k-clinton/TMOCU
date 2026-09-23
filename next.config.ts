f71a754aa403dd13c742f683b426efb1ced5a190
Clinton Omondi <kingclinn40931@gmail.com>
2026-09-23 10:59:00 +0300

    feat: configure security headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [75, 85],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
